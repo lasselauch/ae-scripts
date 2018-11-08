b4db0485846626d3cec79f104e66b1dce8bbb6f1
Lasse Lauch <44460746+lasselauch@users.noreply.github.com>
2018-11-08 15:55:23 +0100

    only if Layer instanceof CompItem

--- add-selected-precomps-to-renderqueue.jsx
+++ add-selected-precomps-to-renderqueue.jsx
@@ -11,7 +11,8 @@
         var numLayers = layers.length;
         for (var l = 0; l < numLayers; l++) {
             var layer = layers[l];
-            app.project.renderQueue.items.add(layer.source);
+            if (layer.source instanceof CompItem){
+                app.project.renderQueue.items.add(layer.source);}
         }
     } catch(err) {
         alert(err);
